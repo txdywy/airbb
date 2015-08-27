0aa8f77597b84ef35a73e59781ad717447b71526
Yi Wei <genius.ron@gmail.com>
2015-08-27 17:21:16 +0800

    fix beijing

--- static/m/js/scripts.js
+++ static/m/js/scripts.js
@@ -5,7 +5,7 @@ google.maps.event.addDomListener(window, 'load', initialize);
 function initialize() {
 
   /* position Amsterdam */
-  var latlng = new google.maps.LatLng(52.3731, 4.8922);
+  var latlng = new google.maps.LatLng(29.7604267, -95.3698028);
 
   var mapOptions = {
     center: latlng,
