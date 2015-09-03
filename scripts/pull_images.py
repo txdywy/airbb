78854c5d1dae8f1d698254b038a26aa3a950d446
mac <geniusron@gmail.com>
2015-09-03 14:30:07 +0800

    fix

--- scripts/pull_images.py
+++ scripts/pull_images.py
@@ -1,13 +1,15 @@
 from models.model_airbb import *
 from misc import airbnb
-cs = City.query.all()
-for c in cs:
-    if c.abbr=='la' or c.id==1:
-        pass
-    else:
-        r = airbnb.get_imgs(city=c.name)
-        hs = House.query.filter_by(city_id=c.id).all()
-        for h in hs:
-            h.img = r[h.id-hs[0].id]
-            flush(h)
+
+def pull():
+    cs = City.query.all()
+    for c in cs:
+        if c.abbr=='la' or c.id==1:
+            pass
+        else:
+            r = airbnb.get_imgs(city=c.name)
+            hs = House.query.filter_by(city_id=c.id).all()
+            for h in hs:
+                h.img = r[h.id-hs[0].id]
+                flush(h)
 
