2ff60b5912ef13fdce066804b2d2c1265962d43a
Yi Wei <genius.ron@gmail.com>
2015-09-21 15:07:42 +0800

    up db and com

--- models/lanner_db.py
+++ models/lanner_db.py
@@ -118,6 +118,218 @@ flush(c)
 
 
 
+c=Community()
+c.lat = '29.82952'
+c.lng = '-95.73049'
+c.city_id = 6
+c.title = 'Raintree Village (Gulf Coast Collection)'
+c.info = """Raintree Village is a beautiful community located in Katy, TX - just west of downtown Houston. Residents enjoy many local attractions, amenities, entertainment and abundant shopping nearby. Students attend the award-winning Katy Independent School District."""
+c.total_price = '$163 - $229'
+c.square = '1,230 - 2,816 sq.ft.'
+c.house_type = 'Single Family'
+c.zipcode = '77449'
+c.address = '20618 Myrtle Rain Court   Katy, TX 77449'
+c.img = 'http://www.lennar.com/images/com/images/new-homes/18/28/mpc_409/mhi/1200x540-OPTMD-Raintree-Village-Monument.jpg'
+c.url = 'http://www.lennar.com/new-homes/texas/houston/katy/raintree-village'
+flush(c)
+
+
+
+
+
+c=Community()
+c.lat = '29.81966'
+c.lng = '-94.99395'
+c.city_id = 6
+c.title = 'Bay River Colony (Brookstone and Wildflower Collections)'
+c.info = """You'll feel right at home in this charming community, located in booming Baytown, TX- just 30 minutes from Downtown Houston.  Bay River Colony is a beautiful community that offers an abundance of local attractions such as the San Jacinto Battleground State Historic Site,  Pirate's Bay Water Park, Stallworth Stadium and the San Jacinto Mall.  Residents here can enjoy serene living while maintaining easy access to big city attractions, major employers - including the Houston Ship Channel, and convenient location to major commuting routes."""
+c.total_price = '$171 - $237'
+c.square = '1,447 - 2,827 sq.ft.'
+c.house_type = 'Single Family'
+c.zipcode = '77521'
+c.address = '4407 Amaryllis Avenue   Baytown, TX 77521'
+c.img = 'http://www.lennar.com/images/com/images/new-homes/18/28/mpc_3/mhi/1200x540.jpg'
+c.url = 'http://www.lennar.com/new-homes/texas/houston/baytown/bay-river-colony'
+flush(c)
+
+
+
+
+
+c=Community()
+c.lat = '29.78579'
+c.lng = '-95.82440'
+c.city_id = 6
+c.title = 'WoodCreek Reserve (Vista Collection)'
+c.info = """The beautiful established community of WoodCreek, located in the Katy area, offers residents quick access to I-10, the Grand Parkway, and the Sam Houston Tollway, making commuting a breeze. With an abundance of community amenities such as an on-site Recreation center with pool and cabanas, a playground, tennis courts and sports fields, a sparkling lake and a stunning greenbelt system, residents of this family friendly community enjoy many opportunities for activities and recreation inside the community.  Locally, a wide variety of shopping, dining, entertainment, and recreation choices complete the picture of a perfect community.
+"""
+c.total_price = 'New homes coming soon'
+c.square = 'sq.ft.'
+c.house_type = 'Single Family'
+c.zipcode = '77494'
+c.address = 'Model Home Coming Soon   Katy, TX 77494'
+c.img = 'http://www.lennar.com/images/com/images/new-homes/18/28/mpc_146/mhi/1200x540-Vista-Sierra-Mesa-4964-A.jpg'
+c.url = 'http://www.lennar.com/new-homes/texas/houston/katy/woodcreek-reserve'
+flush(c)
+
+
+
+
+
+
+
+c=Community()
+c.lat = '29.67365'
+c.lng = '-95.13848'
+c.city_id = 6
+c.title = 'Red Bluff (Stone Twin Villas)'
+c.info = """Lennar builds the beautiful and affordable Stone Twin Villas Collection in the scenic community of Red Bluff in Pasadenda, TX.  These duplex villa homes offer residents home ownership on a smaller plateau with two beautiful Everything's Included floor plans to choose from. These homes are perfectly suited for all family sizes and offer new home buyers convenience to Sam Houston Tollway and I-45.  Your family will love the included amenities such as loft spaces, island kitchens, and spacious master suites.  For more information, please call our knowledgeable New Home Consultants to help you discover your new dream home by Lennar at Red Bluff: Stone Twin Villas!"""
+c.total_price = '$149 - $161'
+c.square = '1,423 - 1,559 sq.ft.'
+c.house_type = 'Single Family'
+c.zipcode = '77503'
+c.address = 'Sales Trailer: 2612 Fern Creek Drive   Pasadena, TX 77503'
+c.img = 'http://www.lennar.com/images/com/images/new-homes/18/28/mpc_514/mhi/1200x540-Red-Bluff-Comm-Entrnc-PRINT-03.jpg'
+c.url = 'http://www.lennar.com/new-homes/texas/houston/pasadena/red-bluff'
+flush(c)
+
+
+
+
+
+c=Community()
+c.lat = '29.51712'
+c.lng = '-95.75037'
+c.city_id = 6
+c.title = 'Walnut Creek (Brookstone Collection)'
+c.info = """Walnut Creek is located just off Highway 59 in the historic Richmond-Rosenberg area. Residents will enjoy the tranquility of natural surroundings and outstanding convenience. Visit the beautifully designed and decorated Lennar model home open daily."""
+c.total_price = '$198 - $258'
+c.square = '1,620 - 2,827 sq.ft.'
+c.house_type = 'Single Family'
+c.zipcode = '77469-5739'
+c.address = '6730 Ashland Terrace Lane   Rosenberg, TX 77469-5739'
+c.img = 'http://www.lennar.com/images/com/images/new-homes/18/28/mpc_75/mhi/1200x540WalnutCreekPRINT-01.jpg'
+c.url = 'http://www.lennar.com/new-homes/texas/houston/rosenberg/walnut-creek'
+flush(c)
+
+
+
+
+c=Community()
+c.lat = '29.53704'
+c.lng = '-95.48957'
+c.city_id = 6
+c.title = 'Winfield Lakes (Brookstone Collection)'
+c.info = """Winfield Lakes is a gorgeous community located in Fresno, TX. Commuting to work or navigating around town is a breeze with easy access to Highway 288, Fort Bend Toll Road and Highway 6. Students in Winfield Lakes attend Fort Bend ISD schools. We invite you to visit this family friendly community and to stop into our Welcome Home Center where our knowledgeable New Home Consultants can show you the Everything's Included home of your dreams in Winfield Lakes today!"""
+c.total_price = '$175 - $230'
+c.square = '1,620 - 2,827 sq.ft.'
+c.house_type = 'Single Family'
+c.zipcode = '77545'
+c.address = '2435 Inlet Court   Fresno, TX 77545'
+c.img = 'http://www.lennar.com/images/com/images/new-homes/18/28/mpc_79/mhi/1200x540-Winfield-Lakes-Fountain.jpg'
+c.url = 'http://www.lennar.com/new-homes/texas/houston/fresno/winfield-lakes'
+flush(c)
+
+
+
+
+
+c=Community()
+c.lat = '29.46274'
+c.lng = '-95.35799'
+c.city_id = 6
+c.title = 'Sedona Lakes (Vista Collection)'
+c.info = """Sedona Lakes is a growing, family-friendly community in a great location just minutes from Houston's major employment corridors, and an abundance of local amenities and attractions, such as local and nearby public parks, movie cinemas, dining venues, and premier shopping areas.  Students attend the award-winning Alvin Independent School District, where the curriculum, instruction, and extracurricular activities along with outstanding community involvement, create a perfect place for all families. Sedona Lakes is quickly becoming a destination spot for new homeowners who are looking for ideal location and amenities. """
+c.total_price = 'New homes coming soon'
+c.square = 'sq.ft.'
+c.house_type = 'Single Family'
+c.zipcode = '77578'
+c.address = 'Model Home Coming Soon   Manvel, TX 77578'
+c.img = 'http://www.lennar.com/images/com/images/new-homes/18/28/mpc_539/mhi/1200x540-Vista-Sierra-Mesa-4964-A.jpg'
+c.url = 'http://www.lennar.com/new-homes/texas/houston/manvel/sedona-lakes'
+flush(c)
+
+
+
+
+c=Community()
+c.lat = '29.94870'
+c.lng = '-95.44109'
+c.city_id = 6
+c.title = 'Magnolia Creek (Vista Collection)'
+c.info = """This family oriented community offers the relaxed, recreational lifestyle you and your family will love, with fantastic on-site resident only recreational facilities, stunning landscaping, and scenic golf course views.  Magnolia Creek features a 27-hole golf course, an enclave of water activities, private fitness and gathering spaces, as well as extensive walking trails that run throughout the neighborhood, and connect to the beautiful, nearby Countryside Park and stunning homes."""
+c.total_price = 'New homes coming soon'
+c.square = 'sq.ft.'
+c.house_type = 'Golf/Single Family'
+c.zipcode = ''
+c.address = 'Model Home Coming Soon   Houston, TX 77067'
+c.img = 'http://www.lennar.com/images/com/images/new-homes/18/28/mpc_535/mhi/1200x540-Magnolia-Crk-Clubhouse-PRINT-03.jpg'
+c.url = 'http://www.lennar.com/new-homes/texas/houston/houston/magnolia-creek'
+flush(c)
+
+
+c=Community()
+c.lat = '29.44509'
+c.lng = '-95.10667'
+c.city_id = 6
+c.title = 'The Trails at Bay Colony (Brookstone Collection)'
+c.info = """The Trails at Bay Colony is a beautiful, family oriented community located in Dickinson, TX. that boasts plenty of family friendly community and local amenities.  Residents enjoy a lovely community park and a wealth of nearby recreational activities, as well as easy access to I-45. Some of the spectacular local amenities include the NASA Space Center, Mall of Mainland, Kemah Boardwalk, and Galveston Island, which is only a short drive away. Visit us today and see all that the Trails of Bay Colony has to offer!"""
+c.total_price = '$195 - $254'
+c.square = '1,620 - 2,827 sq.ft.'
+c.house_type = 'Single Family'
+c.zipcode = '77539'
+c.address = '6827 Linden Creek Lane   Dickinson, TX 77539'
+c.img = 'http://www.lennar.com/images/com/images/new-homes/18/28/mpc_343/mhi/baycolonyPRINT01_1200x540.jpg'
+c.url = 'http://www.lennar.com/new-homes/texas/houston/dickinson/the-trails-at-bay-colony'
+flush(c)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+c=Community()
+c.lat = '29.36857'
+c.lng = '-94.97131'
+c.city_id = 6
+c.title = 'Delany Cove (Wildflower Collection)'
+c.info = """Welcome home to one of Lennar's newest and fantastic new southeast Houston communities.  Located in La Marque, Texas, which is often referred to as "The Gateway to the Gulf",  Lennar builds the beautiful and affordable Brookstone and Fairfield Collections of homes. These homes are perfectly suited for all family sizes and residents can enjoy nearby exciting community attractions which include the Kemah Boardwalk, Stewart Beach, the historic Strand shopping district, the Johnson Space Center, and the Tanger Outlet Mall."""
+c.total_price = '$164 - $245'
+c.square = '1,447 - 2,827 sq.ft.'
+c.house_type = 'Single Family'
+c.zipcode = '77568'
+c.address = 'Model Home Coming Soon   La Marque, TX 77568'
+c.img = 'http://www.lennar.com/images/com/images/new-homes/18/28/mpc_494/mhi/1200x540-Delany-Cove-Community-Park-PRINT-05.jpg'
+c.url = 'http://www.lennar.com/new-homes/texas/houston/la-marque/delany-cove'
+flush(c)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
