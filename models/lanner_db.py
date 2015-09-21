84122f51d7aec4c709d0e59c0fc304c50e508707
Yi Wei <genius.ron@gmail.com>
2015-09-21 14:28:45 +0800

    fix more

---
+++ models/lanner_db.py
@@ -0,0 +1,127 @@
+c=Community()
+c.lat = ''
+c.lng = ''
+c.city_id = 6
+c.title = ' ()'
+c.info = """"""
+c.total_price = ''
+c.square = ''
+c.house_type = ''
+c.zipcode = ''
+c.address = ''
+c.img = ''
+c.url = ''
+flush(c)
+
+
+c=Community()
+c.lat = '30.41276'
+c.lng = '-95.44453'
+c.city_id = 6
+c.title = 'Texas National-Brookstone Collection'
+c.info = "Lennar builds the beautiful and affordable Brookstone Collection in the scenic community of Texas National. You'll love the included amenities such as a kitchen with a breakfast nook, an inviting family room, a relaxing covered patio, and roomy walk-in closets!"
+c.total_price = '$198 - $256'
+c.square = '1,620 - 2,827 sq.ft.'
+c.house_type = 'Single Family'
+c.zipcode = '77378'
+c.address = '12001 Mustang Avenue   Willis, TX 77378'
+c.img = 'http://www.lennar.com/images/com/images/new-homes/18/28/2204/mhi/A11699_68_Lennar%20Houston_BrookStone_Obsidian_3744%20Ele%20E_FINAL_1200x650.jpg'
+c.url = 'http://www.lennar.com/new-homes/texas/houston/willis/texas-national-brookstone-collection'
+flush(c)
+
+
+c=Community()
+c.lat = '30.32676'
+c.lng = '-95.42146'
+c.city_id = 6
+c.title = 'Cedar Woods (Brookstone and Fairfield Collections)'
+c.info = """Cedar Woods is a quiet and charming community in an prime location. It's located just minutes away from downtown Conroe, sparkling Lake Conroe, the buzz of The Woodlands Township, and I-45 making your commute a breeze! Shopping and dining choices are abundant in the nearby Lake Conroe area and The Woodlands. Visit our knowledgable New Home Consultants at our gorgeous Welcome Home Center in Cedar Woods to discover your "Everything's Included" dream home today!"""
+c.total_price = '$175 - $242'
+c.square = '1,424 - 3,252 sq.ft.'
+c.house_type = 'Single Family'
+c.zipcode = '77301'
+c.address = '2308 Ivy Wall Drive   Conroe, TX 77301'
+c.img = 'http://www.lennar.com/images/com/images/new-homes/18/28/mpc_492/mhi/1200x540-Cedar-Woods-Monument%202.jpg'
+c.url = 'http://www.lennar.com/new-homes/texas/houston/conroe/cedar-woods'
+flush(c)
+
+
+
+c=Community()
+c.lat = '30.09716'
+c.lng = '-95.61605'
+c.city_id = 6
+c.title = 'Wildwood at Northpointe (Brookstone Collection)'
+c.info = """Wildwood at Northpointe is a great community to build your home and dreams. Located north of Houston in Tomball, TX, Wildwood is a prime location for business, entertainment, and family fun. You're only minutes away from great recreational activities. If you love golf, you have three great courses to choose from: Tomball Country Club, Windrose Golf Club, and Houston Oaks Golf Club. Visit this amazing community today to see all it has to offer!  New Phase 1 of Tomball Tollway now open."""
+c.total_price = '$198 - $270'
+c.square = '1,424 - 2,713 sq.ft.'
+c.house_type = 'Single Family'
+c.zipcode = '77377'
+c.address = 'Please call for appointments   Tomball, TX 77377'
+c.img = 'http://www.lennar.com/images/com/images/new-homes/18/28/mpc_137/mhi/1200x540-OPTM-WWANP-Fountain.jpg'
+c.url = 'http://www.lennar.com/new-homes/texas/houston/tomball/wildwood-at-northpointe'
+flush(c)
+
+
+
+c=Community()
+c.lat = '29.97569'
+c.lng = '-95.39684'
+c.city_id = 6
+c.title = 'Meadowview Farms (Brookstone and Fairfield Collections)'
+c.info = """Meadowview Farms offers new, affordable homes just north of Beltway 8 and east of I-45, only minutes from downtown destinations and The Woodlands. Local attractions include Willowbrook Mall, Wet & Wild Splash Town, and Old Town Spring."""
+c.total_price = '$158 - $235'
+c.square = '1,424 - 2,827 sq.ft.'
+c.house_type = 'Single Family'
+c.zipcode = '77073'
+c.address = '19503 Shelby Ridge Lane   Houston, TX 77073'
+c.img = 'http://www.lennar.com/images/com/images/new-homes/18/28/mpc_47/mhi/A11699_72_Lennar-Houston_BrookStone-3754-Ele-D_WEB-NEW.jpg'
+c.url = 'http://www.lennar.com/new-homes/texas/houston/houston/meadowview-farms'
+flush(c)
+
+
+
+
+
+c=Community()
+c.lat = '29.91189'
+c.lng = '-95.06215'
+c.city_id = 6
+c.title = 'Newport Seven Oaks (Brookstone Collection)'
+c.info = """Newport Seven Oaks is a quiet, tranquil new home community located in the northeast Houston area, just minutes away from all the bustling activity of the city, and offering residents a relaxing community environment where the sights and sounds of nature become an interwoven part of daily life.  Lennar offers a multitude of new home plans at an affordable price in this community, that included amenities you and your family will love. Visit our knowledgable New Home Consultants at our gorgeous Welcome Home Centers in Newport Seven Oaks to discover your Everything's Included dream home today!"""
+c.total_price = '$203 - $262'
+c.square = '1,620 - 2,827 sq.ft.'
+c.house_type = 'Golf/Single Family'
+c.zipcode = '77532'
+c.address = '16026 Audie Lee Court   Crosby, TX 77532'
+c.img = 'http://www.lennar.com/images/com/images/new-homes/18/28/mpc_512/mhi/1200x540-Newport-Community-PRINT-03.jpg'
+c.url = 'http://www.lennar.com/new-homes/texas/houston/crosby/newport-seven-oaks'
+flush(c)
+
+
+
+
+c=Community()
+c.lat = '29.87907'
+c.lng = '-95.45157'
+c.city_id = 6
+c.title = 'Mount Royal Village (Fairfield and Gulf Coast Collections)'
+c.info = """In the heart of Houston, you'll find and fall in love with Mount Royal Village. Conveniently located near I-45, residents have easy access to work and pleasure. Students attend the Aldine Independent School District. Community amenities include Willowbrook Mall, Sam Houston Raceway, Meyer Park, Jersey Meadow Golf Club, Cypress Creek YMCA, and Mountasia Family Fun Center. Visit Mount Royal Village today!"""
+c.total_price = '$141 - $215'
+c.square = '1,230 - 3,252 sq.ft.'
+c.house_type = 'Single Family'
+c.zipcode = '77088'
+c.address = 'Please call for information   Houston, TX 77088'
+c.img = 'http://www.lennar.com/images/com/images/new-homes/18/28/mpc_48/mhi/1200x540-Surfside-Gulf-Coast-3112-Ele-C_WEB-NEW.jpg'
+c.url = 'http://www.lennar.com/new-homes/texas/houston/houston/mount-royal-village'
+flush(c)
+
+
+
+
+
+
+
+
+
+
